2dd84a33c318457362774694458c1850c088e50f
Axel Stahl <axelkeizo@gmail.com>
2023-04-11 11:34:18 -0400

    z-indexing works now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,52 @@ const inter = Inter({ subsets: ['latin'] })
 
 
 function Home() {
+  const [news1ontop, setnews1ontop] = useState(false);
+  const [news2ontop, setnews2ontop] = useState(false);
+  const [news3ontop, setnews3ontop] = useState(false);
+  const [mag1ontop, setmag1ontop] = useState(false);
+  const [mag2ontop, setmag2ontop] = useState(false);
+  const handleClick = (id: string) => {
+    switch(id) {
+      case "news1":
+        setmag2ontop(false);
+        setmag1ontop(false);
+        setnews1ontop(true);
+        setnews2ontop(false);
+        setnews3ontop(false);
+        break;
+      case "news2":
+        setmag2ontop(false);
+        setmag1ontop(false);
+        setnews1ontop(false);
+        setnews2ontop(true);
+        setnews3ontop(false);
+        break;
+      case "news3":
+        setmag2ontop(false);
+        setmag1ontop(false);
+        setnews1ontop(false);
+        setnews2ontop(false);
+        setnews3ontop(true);
+        break;
+      case "mag1":
+        setmag2ontop(false);
+        setmag1ontop(true);
+        setnews1ontop(false);
+        setnews2ontop(false);
+        setnews3ontop(false);
+        break;
+      case "mag2":
+        setmag2ontop(true);
+        setmag1ontop(false);
+        setnews1ontop(false);
+        setnews2ontop(false);
+        setnews3ontop(false);
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <>
       <div id="hometitle">
@@ -17,19 +63,27 @@ function Home() {
         <h1 id="freshmancaucus">Freshman Causus</h1>
       </div>
       <div id="upcoming">
-        <div id="news1">News</div>
-        <div id="news2">News</div>
-        <div id="news3">News</div>
-        <div id="mag1">Mag</div>
-        <div id="mag2">Mag</div>
-        <div id="mag3">Mag</div>
+        <div  id="news1" className={`${news1ontop ? 'on-top' : ''}`} onClick={() => handleClick('news1')}>News</div>
+        <div id="news2" className={`${news2ontop ? 'on-top' : ''}`} onClick={() => handleClick('news2')}>News</div>
+        <div id="news3" className={`${news3ontop ? 'on-top' : ''}`} onClick={() => handleClick('news3')}>News</div>
+        <div id="mag1" className={`${mag1ontop ? 'on-top' : ''}`} onClick={() => handleClick('mag1')}>Mag</div>
+        <div id="mag2" className={`${mag2ontop ? 'on-top' : ''}`} onClick={() => handleClick('mag2')}>Mag</div>
+        <div id="mag3" >Mag</div>
         <div id="mag4">Mag</div>
       </div>
       <div id="event">
         <h1 id="event-title">Events</h1>
-        <ul>
-          <li>yur mom</li>
-        </ul>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
+        <p>an event</p>
       </div>
     </>
   );
