d175e0ccede8c673ac3ceb37c9e914e336c9d666
Axel Stahl <axelkeizo@gmail.com>
2023-04-14 14:51:48 -0400

    faq

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,51 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { useState } from "react";
-import Faq from "react-faq-component";
+import { useState, useEffect } from "react";
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-const faqs = [
-  {
-    question: 'What is JavaScript?',
-    answer:
-      'JavaScript is a programming language used for creating dynamic and interactive web pages. It is supported by all major web browsers and can also be used for server-side programming.'
-  },
-  {
-    question: 'What is React?',
-    answer:
-      'React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.'
-  },
-  {
-    question: 'What is TypeScript?',
-    answer:
-      'TypeScript is a superset of JavaScript that adds optional static typing and other features to the language. It is maintained by Microsoft and open-source contributors.'
-  },
-  {
-    question: 'What is CSS?',
-    answer:
-      'CSS (Cascading Style Sheets) is a language used for describing the presentation of web pages, including colors, layout, and fonts. It is used in conjunction with HTML and JavaScript to build web applications.'
-  },
-  {
-    question: 'What is HTML?',
-    answer:
-      'HTML (Hypertext Markup Language) is a markup language used for creating web pages and other documents that are viewed in a web browser. It defines the structure and content of a web page using a series of elements and attributes.'
-  }
-];
-
-const style = {
-  top: "-2000px",
-  bgColor: 'white',
-  arrowColor: "red",
-};
-
-const config = {
-  animate: true,
-  arrowIcon: "V",
-  tabFocus: true
-};
 
 function Home() {
   const [news1ontop, setnews1ontop] = useState(false);
@@ -54,6 +14,13 @@ function Home() {
   const [news3ontop, setnews3ontop] = useState(false);
   const [mag1ontop, setmag1ontop] = useState(false);
   const [mag2ontop, setmag2ontop] = useState(false);
+  const [faq1, setfaq1] = useState(1)
+  const [faq2, setfaq2] = useState(2)
+  const [faq3, setfaq3] = useState(3)
+  const [faq4, setfaq4] = useState(3)
+  const [faq5, setfaq5] = useState(3)
+  const [faq6, setfaq6] = useState(3)
+  const [faq7, setfaq7] = useState(3)
   const handleClick = (id: string) => {
     switch(id) {
       case "news1":
@@ -95,6 +62,45 @@ function Home() {
         break;
     }
   }
+
+  function switchstack1(j) {
+    switch (j) {
+      case 'faq4':
+        setfaq4(2);
+        break;
+      case 'faq5':
+        setfaq5(1);
+        break;
+      case 'faq6':
+        setfaq6(2);
+        break;
+      case 'faq7':
+        setfaq7(1);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function switchstack2(j) {
+    switch (j) {
+      case 'faq4':
+        setfaq4(3);
+        break;
+      case 'faq5':
+        setfaq5(3);
+        break;
+      case 'faq6':
+        setfaq6(3);
+        break;
+      case 'faq7':
+        setfaq7(3);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <>
       <div id="hometitle">
@@ -124,19 +130,18 @@ function Home() {
         <p>an event</p>
         <p>an event</p>
       </div>
-      <div>
-      {faqs.map((faq, index) => (
-          <div
-            key={faq.question}
-            className={`faq`}
-          >
-            <div className="faq-question">
-              {faq.question}
-              <span className="faq-arrow"></span>
-            </div>
-            <div className="faq-answer">{faq.answer}</div>
-          </div>
-        ))}
+      <div id="FaQ">
+        <div id="linebreak"></div>
+        <h2 id="FaQtitle">F A Q</h2>
+        <div id="faqholder">
+          <div className={`${faq1==1 ? 'stack1' : faq1==2 ? 'stack2' : faq1==3 ? 'stack3' : ''} faq faq1`}>faq1</div>
+          <div className={`${faq2==1 ? 'stack1' : faq2==2 ? 'stack2' : faq2==3 ? 'stack3' : ''} faq faq2`}>faq2</div>
+          <div className={`${faq3==1 ? 'stack1' : faq3==2 ? 'stack2' : faq3==3 ? 'stack3' : ''} faq faq3`} >faq3</div>
+          <div className={`${faq4==1 ? 'stack1' : faq4==2 ? 'stack2' : faq4==3 ? 'stack3' : ''} faq faq4`} onClick={faq4==3 ? () => {switchstack1('faq4')} : () => {switchstack2("faq4")}}>faq4</div>
+          <div className={`${faq5==1 ? 'stack1' : faq5==2 ? 'stack2' : faq5==3 ? 'stack3' : ''} faq faq5`} onClick={faq5==3 ? () => {switchstack1('faq5')} : () => {switchstack2("faq5")}}>faq5</div>
+          <div className={`${faq6==1 ? 'stack1' : faq6==2 ? 'stack2' : faq6==3 ? 'stack3' : ''} faq faq6`} onClick={faq6==3 ? () => {switchstack1('faq6')} : () => {switchstack2("faq6")}}>faq6</div>
+          <div className={`${faq7==1 ? 'stack1' : faq7==2 ? 'stack2' : faq7==3 ? 'stack3' : ''} faq faq7`} onClick={faq7==3 ? () => {switchstack1('faq7')} : () => {switchstack2("faq7")}}>faq7</div>
+        </div>
       </div>
     </>
   );
